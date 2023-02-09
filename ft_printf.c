be6edec0440198a1d8d2aa8f2a9a9c89e3a4dd37
Moe Ota <moeota@c1r23s7.42tokyo.jp>
2023-02-09 16:45:19 +0900

    111

--- ft_printf.c
+++ ft_printf.c
@@ -79,9 +79,8 @@ int divided_case(char *str,  va_list args, int check_tmp , int *count)
 	else if (*spec == 'u')
 	{
 		unsigned int num;
-		// int cnt;
 		num = va_arg(args,unsigned int);
-		ft_putnbr(num,1);
+		ft_putnbr_u(num,1);
 		return (digit_count(num));
 
 	}
@@ -199,7 +198,7 @@ int ft_printf(const char *str, ...)
 			return_length += divided_case(find, args, check_tmp, &count);
 			find += count;
             // printf("%d\n",count);
-			va_arg(args,int);
+			// va_arg(args,int);
 		}
 	}
 
@@ -208,135 +207,149 @@ int ft_printf(const char *str, ...)
 	return (return_length);
 }
 
-// #include <limits.h>
-// int main()
-// {
-// 	// char *memo = "arg_2:%p";
-//     // int  a = '3';//test1
-// 	// char *a = "12345";
-// 	// int *num = 123456;
-// 	int d;
-// 	int c;
-//     // int a;
-// 	char *p;
+#include <limits.h>
+int main()
+{
+	// char *memo = "arg_2:%p";
+    // int  a = '3';//test1
+	// char *a = "12345";
+	// int *num = 123456;
+	int d;
+	int c;
+    // int a;
+	char *p;
 
 
-// 	int a = 10;
-// 	// int b = 100000;
-// 	printf("%u", -100);
-// 	ft_printf("%u", -100);
-// 	printf("[%14d]\n", a);
+	int a = 10;
+	// int b = 100000;
+	// printf("%u", -100);
+	// ft_printf("%u", -100);
+	// printf("[%14d]\n", a);
 
-// 	// ft_printf("-------------------------%d",1);
-// 	// p = "12345";
-// 	// unsigned int num2 = -234567;
-//     // double arg_3 = 3.14;
-//   	// pritnf("set1:%c",a);
-// 	// printf("helloeworld%n",&d);
-// 	// c = ft_printf("helloeworld%n",&d);
-// 	// int n  = printf("helloeworld%n",&d);
-// 	// // printf("i = %d" , d);
-// 	// ft_printf("i = %d" , d);
-// 	// printf("%d\n",c);
-// 	// printf("n:%d\n",n);
+	// printf(" %c %c %c ", '0', 0, '1');
 
-//     // c = printf("%+d\n", 1);
-// 	// printf("%d\n",c);
+	printf(" %c %c %c ", ' ', ' ', ' ');
 
-//     // ft_printf("%+d\n", 1);
-// 	// ft_printf("%d\n",a);
+	// printf(" %c %c %c ", '1', '2', '3');
 
-// 	// printf("-----------------answer1----------------\n");
-// 	// ft_printf("set1:%p--------",p);
-// 	// d = printf("set1:%pans\n",num);
-// 	// printf("set1:%d\n",num);
-// 	// d = printf(" %c %c %c ", '0', 0, '1');
-//     // c  = printf(" %d ", -9);
-//     // d = printf("%d ", num);
-// 	// c = -123456;
-// 	// d = printf("%x",-1);
-// 	// d = printf("%x",-2);
-// 	// d = printf("%x",-3);
+	// printf(" %c %c %c ", '2', '1', 0);
+	// printf(" %c %c %c ", 0, '1', '2');
 
+	// ft_printf(" %c %c %c ", '0', 0, '1');
+	ft_printf("answer;%c %c %c ", 'a', 'a', 'a');
+	ft_printf(" %c %c %c ", '1', '2', '3');
+	ft_printf(" %c %c %c ", '2', '1', 0);
+	// ft_printf(" %c %c %c ", 0, '1', '2');
+	// ft_printf("-------------------------%d",1);
+	// p = "12345";
+	// unsigned int num2 = -234567;
+    // double arg_3 = 3.14;
+  	// pritnf("set1:%c",a);
+	// printf("helloeworld%n",&d);
+	// c = ft_printf("helloeworld%n",&d);
+	// int n  = printf("helloeworld%n",&d);
+	// // printf("i = %d" , d);
+	// ft_printf("i = %d" , d);
+	// printf("%d\n",c);
+	// printf("n:%d\n",n);
 
-//     // printf("%#o\n", 10);
-//     // printf("%#o\n", 0);
-//     // d = printf("%x\n", 10);
-// 	// printf("%d\n",d);
-//     // d = ft_printf("%x\n", 10);
-// 	// printf("%d\n",d);
-//     // d = printf("%#x\n", 10);
-// 	// printf("%d\n",d);
-//     // d = ft_printf("%#x\n", 10);
-// 	// printf("%d\n",d);
-//     // d = printf("%#x\n", 0);
-// 	// printf("%d\n",d);
-//     // d = ft_printf("%#x\n", 0);
-// 	// // printf("%d\n",d);
-// 	// printf("-------------------\n");
-//     // printf("%X\n", 10);
-//     // ft_printf("%X\n", 10);
-//     // printf("%#X\n", 10);
-//     // ft_printf("%#X\n", 10);
-//     // printf("%#X\n", 0);
-//     // ft_printf("%#X\n", 0);
+    // c = printf("%+d\n", 1);
+	// printf("%d\n",c);
 
-// 	// printf("%d\n", 1);
-// 	// printf("%d\n", 1);
-// 	// printf("%d\n", -1);
-// 	// printf("%d\n", -1);
+    // ft_printf("%+d\n", 1);
+	// ft_printf("%d\n",a);
+
+	// printf("-----------------answer1----------------\n");
+	// ft_printf("set1:%p--------",p);
+	// d = printf("set1:%pans\n",num);
+	// printf("set1:%d\n",num);
+	// d = printf(" %c %c %c ", '0', 0, '1');
+    // c  = printf(" %d ", -9);
+    // d = printf("%d ", num);
+	// c = -123456;
+	// d = printf("%x",-1);
+	// d = printf("%x",-2);
+	// d = printf("%x",-3);
 
 
-// 	d = printf("% d\n", 1);
-// 	printf("%d\n",d);
-// 	d = ft_printf("% d\n", 1);
-// 	printf("%d\n",d);
+    // printf("%#o\n", 10);
+    // printf("%#o\n", 0);
+    // d = printf("%x\n", 10);
+	// printf("%d\n",d);
+    // d = ft_printf("%x\n", 10);
+	// printf("%d\n",d);
+    // d = printf("%#x\n", 10);
+	// printf("%d\n",d);
+    // d = ft_printf("%#x\n", 10);
+	// printf("%d\n",d);
+    // d = printf("%#x\n", 0);
+	// printf("%d\n",d);
+    // d = ft_printf("%#x\n", 0);
+	// // printf("%d\n",d);
+	// printf("-------------------\n");
+    // printf("%X\n", 10);
+    // ft_printf("%X\n", 10);
+    // printf("%#X\n", 10);
+    // ft_printf("%#X\n", 10);
+    // printf("%#X\n", 0);
+    // ft_printf("%#X\n", 0);
 
-// 	printf("----------------------------\n");
-// 	d = printf("% d\n", -1);
-// 	d = ft_printf("% d\n", -1);
-
-// 	// printf("%+d\n", 1);
-// 	// printf("%+d\n", -1);
-// 	// d = printf("%x",-4);
-// 	// d = printf("%x",-16);
-// 	// d = printf("%x",-17);
-// 	// d = printf("%x",-18);
-// 	// d = printf("%x",-19);
-// 	// d = printf("%x",-20);
-// 	// printf("%d",d);
-// 	// printf("%d", d);
-
-// 	// d = printf("%d\n", INT_MIN);
-// 	// printf("%d", d);
-// 	// // d = ft_printf(" %d ", num);
-
-// 	// printf("d:%d", d);
-
-// 	// d = printf(" %c %c %c ", '1', '2', '3');
-// 	// printf("~~~~%X~~~~~\n",d);
-// 	// printf("-----------------answer2----------------\n");
-// 	// ft_printf("set1:%Xans",num);
-// 	// d = ft_printf("%x", -9);
-// 	// printf("%d",d);
-// 	// d = -100;
-// 	// ft_printf("%d", d);
-// 	// ft_printf("d:%u", d);
-//     // c  = ft_printf(" %d ", -9);
-// 	// d = ft_printf("%d\n", INT_MIN);
-// 	// printf("-----------------answer3----------------\n");
-// 	// ft_printf("%d", d);
-//     // d = ft_printf(" %u\n ", c);
-//     // printf(" %d ",d);
-
-// 	// d = ft_printf(" %c %c %c ", '1', '2', '3');
-// 	// d = ft_printf("%c", '1');
-// 	// ft_printf("%s",p);
-// 	// printf("~~~~%X~~~~~\n",d);
-// 	return (0);
+	// printf("%d\n", 1);
+	// printf("%d\n", 1);
+	// printf("%d\n", -1);
+	// printf("%d\n", -1);
 
 
-// }
+	d = printf("% d\n", 1);
+	printf("%d\n",d);
+	d = ft_printf("% d\n", 1);
+	printf("%d\n",d);
+
+	printf("----------------------------\n");
+	d = printf("% d\n", -1);
+	d = ft_printf("% d\n", -1);
+
+	// printf("%+d\n", 1);
+	// printf("%+d\n", -1);
+	// d = printf("%x",-4);
+	// d = printf("%x",-16);
+	// d = printf("%x",-17);
+	// d = printf("%x",-18);
+	// d = printf("%x",-19);
+	// d = printf("%x",-20);
+	// printf("%d",d);
+	// printf("%d", d);
+
+	// d = printf("%d\n", INT_MIN);
+	// printf("%d", d);
+	// // d = ft_printf(" %d ", num);
+
+	// printf("d:%d", d);
+
+	// d = printf(" %c %c %c ", '1', '2', '3');
+	// printf("~~~~%X~~~~~\n",d);
+	// printf("-----------------answer2----------------\n");
+	// ft_printf("set1:%Xans",num);
+	// d = ft_printf("%x", -9);
+	// printf("%d",d);
+	// d = -100;
+	// ft_printf("%d", d);
+	// ft_printf("d:%u", d);
+    // c  = ft_printf(" %d ", -9);
+	// d = ft_printf("%d\n", INT_MIN);
+	// printf("-----------------answer3----------------\n");
+	// ft_printf("%d", d);
+    // d = ft_printf(" %u\n ", c);
+    // printf(" %d ",d);
+
+	// d = ft_printf(" %c %c %c ", '1', '2', '3');
+	// d = ft_printf("%c", '1');
+	// ft_printf("%s",p);
+	// printf("~~~~%X~~~~~\n",d);
+	return (0);
+
+
+}
 
 
 
